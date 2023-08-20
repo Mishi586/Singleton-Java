0d6845fa0570fe1a338130250de0254d37485045
Mishal Arshad <107922669+Mishi586@users.noreply.github.com>
2023-08-20 17:28:18 +0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,34 @@
+public class PersonEager {
+    private static final PersonEager instance = new PersonEager();
+
+    private int autoId;
+    private String username;
+    private String password;
+    private String gender;
+
+    private PersonEager() {
+       autoId=2;
+      username="abc";
+      password="123";
+      gender="male";
+    }
+
+    public static PersonEager getInstance() {
+      if(instance==null)
+      {
+        instance = new PersonEager();
+        
+    }
+return instance;
+}
+}
+public class Main {
+    public static void main(String[] args) {
+        // Creating an instance of the class
+        PersonEager person = PersonEager.getInstance();
+
+    
+       
+}
+}
+
